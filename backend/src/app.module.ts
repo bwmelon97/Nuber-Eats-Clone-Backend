248e3d5e49949c5fe0ccc7564de0d57d1de14b0e
Soogeun Park <bwmelon97@naver.com>
2021-05-03 13:49:34 +0000

    B: add TypeOrmModule to App Moudle

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExampleModule } from './example/example.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+      logging: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }), 
