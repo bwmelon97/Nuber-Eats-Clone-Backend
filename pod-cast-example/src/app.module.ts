684ed9f3b31366f3f732fe6fc712b9714e8b13cd
Soogeun Park <bwmelon97@naver.com>
2021-05-04 09:20:27 +0000

    pod-cast: generate controller and service of Podcasts

--- pod-cast-example/src/app.module.ts
+++ pod-cast-example/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PodcastsController } from './podcasts/podcasts.controller';
+import { PodcastsService } from './podcasts/podcasts.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PodcastsController],
+  providers: [AppService, PodcastsService],
 })
 export class AppModule {}
