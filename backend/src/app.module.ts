435a9549b7d8ad89d41bb9c22531be4118ac09bc
Soogeun Park <bwmelon97@naver.com>
2021-05-04 14:16:56 +0000

    B: configuring ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExampleModule } from './example/example.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,         // All Code can access config module in everywhere
+      envFilePath: process.env.NODE_ENV === "dev" ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [],
       synchronize: true,
       logging: true
