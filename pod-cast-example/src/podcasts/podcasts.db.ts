d08e9f2dd209dcc4fc2fb7736b50eb45f0bad418
Soogeun Park <bwmelon97@naver.com>
2021-05-04 09:27:18 +0000

    pod-cast: create fake db for podcasts

---
+++ pod-cast-example/src/podcasts/podcasts.db.ts
@@ -0,0 +1,5 @@
+import { Episode } from "./entities/episode.entity";
+import { Podcast } from "./entities/podcast.entity";
+
+export const podcasts: Podcast[] = []
+export const episodes: Episode[] = []
