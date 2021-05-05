83443c50abdba74fa3285338895328a2b39da747
Soogeun Park <bwmelon97@naver.com>
2021-05-05 06:59:54 +0000

    pod-cast: implement GET, CREATE podcast func

--- pod-cast-example/src/podcasts/dtos/createPodcastDTO.ts
+++ pod-cast-example/src/podcasts/dtos/createPodcastDTO.ts
@@ -1,4 +1,10 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+@InputType()
 export class CreatePodcastDTO {
+    @Field(is => String)
     readonly title:     string;
+    
+    @Field(is => String)
     readonly category:  string;
 }
--- pod-cast-example/src/podcasts/podcasts.resolver.ts
+++ pod-cast-example/src/podcasts/podcasts.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreatePodcastDTO } from './dtos/createPodcastDTO';
+import { Podcast } from './entities/podcast.entity';
+import { PodcastsService } from './podcasts.service';
 
 @Resolver()
-export class PodcastsResolver {}
+export class PodcastsResolver {
+
+    private readonly podcastService = new PodcastsService()
+
+    @Query( returns => [Podcast] )
+    podcasts() { return this.podcastService.getAllPodCasts() }
+
+    @Query( returns => Podcast )
+    podcastByID( @Args('id') id: number ) {
+        return this.podcastService.getOnePodCastByID(id)
+    }
+
+    @Mutation( returns => Boolean )
+    createPodcast( 
+        @Args('createPodcastDTO') createPodcastDto: CreatePodcastDTO 
+    ) {
+        return this.podcastService.createPodCast(createPodcastDto)
+    }
+
+    
+}
