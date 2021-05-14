cffc852f6f6dc3f7cde8cf59710e304b7e6784ee
Soogeun Park <bwmelon97@naver.com>
2021-05-14 10:54:58 +0000

    pod-cast: initiate unit test for jwt service

--- pod-cast-example/src/jwt/jwt.service.spec.ts
+++ pod-cast-example/src/jwt/jwt.service.spec.ts
@@ -1,4 +1,26 @@
+import { Test } from "@nestjs/testing"
+import { JwtService } from "./jwt.service"
+
+// const mockJwt = {
+//     sign: jest.fn(),
+//     verify: jest.fn(),
+// }
+
 describe('JwtService', () => {
-    it.todo('Test it all!!');
-  });
-  
+    let service: JwtService
+    // let jwt: typeof mockJwt
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [JwtService]
+        }).compile()
+        service = module.get<JwtService>(JwtService)
+        // jwt = mockJwt
+    })
+
+    it('should be defined', () => {
+        expect(service).toBeDefined()
+    })
+    it.todo('sign')
+    it.todo('verify')
+})
