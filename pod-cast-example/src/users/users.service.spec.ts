898a895035d5f19df7c459a8c40816248d153363
Soogeun Park <bwmelon97@naver.com>
2021-05-14 13:13:12 +0000

    pod-cast: initiate unit-test of users service

--- pod-cast-example/src/users/users.service.spec.ts
+++ pod-cast-example/src/users/users.service.spec.ts
@@ -1,4 +1,52 @@
+import { Test } from "@nestjs/testing"
+import { getRepositoryToken } from "@nestjs/typeorm"
+import { JwtService } from "src/jwt/jwt.service"
+import { Repository } from "typeorm"
+import { User } from "./entities/user.entity"
+import { UsersService } from "./users.service"
+
+const mockRepository = () => ({
+    find: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+})
+
+const mockJwtService = () => ({
+    sign: jest.fn(),
+    verify: jest.fn(),   
+})
+
+type MockRepository<T = any> = Partial< Record< keyof Repository<T>, jest.Mock > >
+
 describe('UsersService', () => {
-    it.todo('Test it all!!');
-  });
-  
+
+    let service: UsersService
+    let users: MockRepository<User>
+    let jwtService: JwtService
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                UsersService,
+                { provide: getRepositoryToken(User), useValue: mockRepository() },
+                { provide: JwtService, useValue: mockJwtService() }
+            ]
+        }).compile()
+
+        service = module.get<UsersService>(UsersService)
+        users = module.get(getRepositoryToken(User))
+        jwtService = module.get<JwtService>(JwtService)
+    })
+
+    it('should be defined', () => {
+        expect(service).toBeDefined()
+    })
+
+    it.todo('findUserById')
+    it.todo('createAccount')
+    it.todo('login')
+    it.todo('editProfile')
+})
