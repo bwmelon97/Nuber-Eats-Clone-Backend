ca77a189eba4d763c7b32f4acd00aaa8ed676f40
Soogeun Park <bwmelon97@naver.com>
2021-05-15 08:26:12 +0000

    b: e2e test: user mo - createUser

--- backend/test/user.e2e-spec.ts
+++ backend/test/user.e2e-spec.ts
@@ -2,6 +2,9 @@ import { INestApplication } from "@nestjs/common"
 import { Test, TestingModule } from "@nestjs/testing"
 import { AppModule } from "src/app.module"
 import { getConnection } from "typeorm"
+import * as request from 'supertest';
+
+const GRAPHQL_ENDPOINT = '/graphql'
 
 describe('UserModule (e2e)', () => {
     let app: INestApplication
@@ -20,10 +23,49 @@ describe('UserModule (e2e)', () => {
         await app.close()
     })
 
+    describe('createUser', () => {
+        const createUserQuery = `
+        mutation {
+            createUser(input: {
+                email: "test@mail.com"
+                password: "test"
+                role: Client
+            }) {
+                ok
+                error
+            }
+        }`
+
+        it('should return status 200 & create an accout', () => {
+            request(app.getHttpServer())
+                .post(GRAPHQL_ENDPOINT)
+                .send({ query: createUserQuery })
+                .expect(200)
+                .expect( res => {
+                    expect(res.body.data.createUser).toEqual({
+                        ok: true,
+                        error: null
+                    })
+                })
+        })
+
+        it('should return status 200 & false if a user exist', () => {
+            request(app.getHttpServer())
+            .post(GRAPHQL_ENDPOINT)
+            .send({ query: createUserQuery })
+            .expect(200)
+            .expect( res => {
+                expect(res.body.data.createUser).toEqual({
+                    ok: false,
+                    error: expect.any(String)
+                })
+            })
+        })
+    })
+    
     it.todo('users')
     it.todo('me')
     it.todo('userProfile')
-    it.todo('createUser')
     it.todo('login')
     it.todo('editProfile')
     it.todo('verifyEmail')
