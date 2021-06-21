1efdf41c001d85a904c321a7d5231d5009e2d23c
Soogeun Park <bwmelon97@naver.com>
2021-06-21 14:12:02 +0000

    B order: code refactoring of checkValidUser for get order logic

---
+++ backend/src/order/repositories/order.repository.ts
@@ -0,0 +1,35 @@
+import { User, UserRole } from "src/user/entities/user.entity";
+import { EntityRepository, Repository } from "typeorm";
+import { GetOrderOutput } from "../dtos/get-order.dto";
+import { Order } from "../entities/order.entity";
+
+@EntityRepository(Order)
+export class OrderRepository extends Repository<Order> {
+
+    async getAndCheckValidUser (orderId: number, user: User): Promise<GetOrderOutput> {
+        try {
+            const order = await this.findOne(orderId, {
+                relations: ['restaurant']
+            })
+            if (!order) throw Error("Couldn't find a order")
+
+            let id: number;
+            switch(user.role) {
+                case UserRole.Client:
+                    id = order.customerId; break;
+                case UserRole.Delivery:
+                    id = order.driverId; break;
+                case UserRole.Owner:
+                    id = order.restaurant.ownerId; break;
+            }
+
+            if ( id !== user.id) 
+                throw Error("You don't have permission to see this order.")
+
+            return { ok: true, order }
+        } catch (error) {
+            return { ok: false, error: error.message }
+        }
+    }
+
+}
