3f77f4d9475f9b7d5bbfbeec49667e79a31b598e
Soogeun Park <bwmelon97@naver.com>
2021-07-03 08:43:01 +0000

    delete example directory

--- example/src/example/dtos/createRestaurant.dto.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, OmitType } from "@nestjs/graphql";
-import { Restaurant } from "../entities/example.entity";
-
-@InputType()
-export class CreateRestaurantDTO extends OmitType(
-    Restaurant, 
-    ['id'], 
-    InputType
-) {}
