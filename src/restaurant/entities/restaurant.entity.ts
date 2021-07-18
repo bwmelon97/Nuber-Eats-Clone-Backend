a2cfb7f111be1175e492d613e03204a688974edf
Soogeun Park <bwmelon97@naver.com>
2021-07-18 07:08:20 +0000

    restaurant entity: set category column as eager true

--- src/restaurant/entities/restaurant.entity.ts
+++ src/restaurant/entities/restaurant.entity.ts
@@ -38,7 +38,7 @@ export class Restaurant extends CoreEntity {
     @ManyToOne( 
         type => Category, 
         category => category.restaurants,
-        { onDelete: 'SET NULL' }
+        { onDelete: 'SET NULL', eager: true }
     )
     category: Category
 
