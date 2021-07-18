08c54836a44f47ae8b493dc7726f45263fb52b3f
Soogeun Park <bwmelon97@naver.com>
2021-07-18 06:49:35 +0000

    fix error of dto gql type

--- src/restaurant/dtos/get-my-restaurants.dto.ts
+++ src/restaurant/dtos/get-my-restaurants.dto.ts
@@ -1,5 +1,9 @@
+import { InputType, ObjectType } from "@nestjs/graphql";
 import { PaginationInput } from "src/common/dtos/pagination.dto";
 import { GetRestaurantsOutput } from "./get-restaurants.dto";
 
+@InputType()
 export class GetMyRestaurantsInput extends PaginationInput {}
+
+@ObjectType()
 export class GetMyRestaurantsOutput extends GetRestaurantsOutput {}
